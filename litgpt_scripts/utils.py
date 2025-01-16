a809c6907d49ad1a1e8d64606d028941dd1e60dd
100daggers <dinesh.soudagar@gmail.com>
2025-01-16 09:25:23 +0100

    Rearranging

---
+++ litgpt_scripts/utils.py
@@ -0,0 +1,9 @@
+import json
+def load_json(json_file_path):
+    """
+    :param json_file_path: Path to json file
+    """
+    with open(json_file_path, "r") as file:
+        data = json.load(file)
+    print("Number of entries:", len(data))
+    return data
